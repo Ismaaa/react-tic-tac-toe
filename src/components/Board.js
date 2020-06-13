046c466e45e8ce24ed7d032ccc5f7307cf122d1a
Isma <didouhismael@gmail.com>
2020-06-13 14:54:24 +0200

    board styling

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,9 +5,19 @@ import React from 'react';
 // components
 import Square from './Square';
 
+const style = {
+  border: '4px solid darkblue',
+  borderRadius: '10px',
+  width: '250px',
+  height: '250px',
+  margin: '0 auto',
+  display: 'grid',
+  gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)',
+};
+
 const Board = ({ onClick }) => {
   return (
-    <>
+    <div style={style}>
       <Square value={1} onClick={() => onClick('dummy value')} />
       <Square value={2} onClick={() => onClick('dummy value')} />
       <Square value={3} onClick={() => onClick('dummy value')} />
@@ -17,7 +27,7 @@ const Board = ({ onClick }) => {
       <Square value={7} onClick={() => onClick('dummy value')} />
       <Square value={8} onClick={() => onClick('dummy value')} />
       <Square value={9} onClick={() => onClick('dummy value')} />
-    </>
+    </div>
   );
 };
 
