b1991e06b4d082926730d36d9fcf112d1774e90b
Isma <didouhismael@gmail.com>
2020-06-13 22:58:42 +0200

    finished game

--- src/components/Board.js
+++ src/components/Board.js
@@ -15,23 +15,20 @@ const style = {
   gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)',
 };
 
-const Board = ({ onClick }) => {
+const Board = ({ squares, onClick }) => {
+  console.log(squares);
+
   return (
     <div style={style}>
-      <Square value={1} onClick={() => onClick('dummy value')} />
-      <Square value={2} onClick={() => onClick('dummy value')} />
-      <Square value={3} onClick={() => onClick('dummy value')} />
-      <Square value={4} onClick={() => onClick('dummy value')} />
-      <Square value={5} onClick={() => onClick('dummy value')} />
-      <Square value={6} onClick={() => onClick('dummy value')} />
-      <Square value={7} onClick={() => onClick('dummy value')} />
-      <Square value={8} onClick={() => onClick('dummy value')} />
-      <Square value={9} onClick={() => onClick('dummy value')} />
+      {squares.map((square, i) => (
+        <Square key={i} value={square} onClick={() => onClick(i)} />
+      ))}
     </div>
   );
 };
 
 Board.propTypes = {
+  squares: PropTypes.array.isRequired,
   onClick: PropTypes.func.isRequired,
 };
 
